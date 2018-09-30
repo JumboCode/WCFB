64a3aafb191d449fc50752243bb61823d0821d7e
jonathanconroy <jconroy@bancroftschool.org>
2018-09-30 14:39:32 -0400

    adding undocumented, untested function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
 function hello() {
   window.alert('hi');
 }
+
+function escapeHtml(unsafe_string) {
+    return unsafe_string
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
