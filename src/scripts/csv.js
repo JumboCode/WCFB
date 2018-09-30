7eadcadcbcf60bd4de163cf00fa3a60c177ee5ab
harshoza24@gmail.com <harshoza24@gmail.com>
2018-09-30 15:56:21 -0400

    Wrote a function to write sample data to csv file

--- src/scripts/csv.js
+++ src/scripts/csv.js
@@ -1,3 +1,22 @@
-function writeCSV() {
-
+function WriteCSV() {
+	var fs = require('fs');
+	const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+	var ws = fs.createWriteStream('Italy.csv');
+	const csvWriter = createCsvWriter({
+	    path: 'Italy.csv',
+	    header: [
+	        {id: 'name', title: 'NAME'},
+	        {id: 'lang', title: 'LANGUAGE'}
+	    ]
+	});
+	 
+	const records = [
+	    {name: 'Bob',  lang: 'French, English'},
+	    {name: 'Mary', lang: 'English'}
+	];
+	 
+	csvWriter.writeRecords(records)       // returns a promise
+	    .then(() => {
+	        console.log('...Done');
+	    });
 }
