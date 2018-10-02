5989507a7dcf7b886d6f77456ea9efa8ac07044d
jonathanconroy <jconroy@bancroftschool.org>
2018-10-01 20:38:23 -0400

    put function in security.js file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,3 @@
 function hello() {
   window.alert('hi');
 }
-
-// Function: sanitize_string
-// Input: String to be sanitized
-// Returns: Sanitized string
-// Does: Sanitizes string
-function sanitize_string(unsafe_string) {
-    return unsafe_string
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
