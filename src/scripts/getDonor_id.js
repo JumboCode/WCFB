b9bbbecb7cb04cc91e4bd975449dcc40b0bd7178
harshoza24@gmail.com <harshoza24@gmail.com>
2018-10-09 13:31:28 -0400

    Added function to get donor id.

---
+++ src/scripts/getDonor_id.js
@@ -0,0 +1,43 @@
+function Dictionary() {
+	this.sets = [];
+
+	this.add = function(name, id) {
+		if (name && id) {
+			this.sets.push({
+				name: name,
+				id: id
+			});
+			return this.sets;
+		}
+	}
+
+	this.findID = function(name) {
+		for (var i = 0; i < this.sets.length; i++) {
+			if (this.sets[i].name == name) {
+				return this.sets[i].id;
+			}
+		}
+		return this.sets;
+	}
+
+	this.removeUser = function(name) {
+		for (var i = 0; i < this.sets.length; i++) {
+			if (this.sets[i].name == name) {
+				this.sets[i].splice(this.sets[i], 1);
+			}
+		}
+		return this.sets;
+	}
+}
+
+
+var workers = new Dictionary();
+workers.add('Bob', 12345);
+workers.add('Bob1', 1232345);
+workers.add('Bob2', 123542345);
+workers.add('Bob3', 1233224345);
+workers.add('Bob4', 12452342345);
+workers.add('Bob5', 123264345);
+workers.add('Bob6', 12372345);
+workers.add('Bob7', 12341345);
+console.log(workers.findID('Bob7'));
