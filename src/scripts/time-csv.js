90859e4c856573ff9a033e3dcee829f4128ce40e
jonathanconroy <jconroy@bancroftschool.org>
2018-10-26 16:57:11 -0400

    update functions

--- src/scripts/time-csv.js
+++ src/scripts/time-csv.js
@@ -1,16 +1,32 @@
+const TIME_TO_CHECK = 86400000;
 
+// Writes the current time into local storage
 function UpdateTimeSinceImport(){
-  var currentTime = new Date();
-  localStorage.setItem("time", currentTime);
-  return;
-
+    var currentTime = new Date();
+    var currentMilliseconds = currentTime.getTime();
+    localStorage.setItem("time", currentMilliseconds);
 }
+
+// Checks whether TIME_TO_CHECK has elapsed since last import
 function CheckLastImport(){
-  console.log(localStorage.getItem("time"));
-  return;
-}
-function DisplayImportPage(){
+    var last_import = localStorage.getItem("time");
+    var curr_time = new Date();
+    var curr_millis = curr_time.getTime();
+
+    return (curr_millis - TIME_TO_CHECK >= last_import);
 }
 
-UpdateTimeSinceImport();
-CheckLastImport();
+// Pulls up import page if needed
+function DisplayImportPage(){
+    var update_page = CheckLastImport();
+
+    if(update_page){
+        //pull up page
+        UpdateTimeSinceImport();
+    }
+    else{
+        //don't do stuff
+    }
+}
+
+DisplayImportPage();
