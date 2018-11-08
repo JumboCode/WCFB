bcee568f5c2b0e2c49c252e9a11999aa7a48700d
harshoza24@gmail.com <harshoza24@gmail.com>
2018-11-08 11:02:47 -0500

    Added function to download csv

--- src/scripts/signout.js
+++ src/scripts/signout.js
@@ -1,3 +1,5 @@
+var testCSV = localStorage.getItem('csv');
+
 var page_state = 0
 
 function val_getter_1(a) {
@@ -85,6 +87,78 @@ function submitForm() {
 		}
 
 		console.log(info)
+		download_csv(testCSV);
 	}
 
 }
+
+function download_csv(csvData) {
+	var hiddenElement = document.createElement('a');
+	hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvData);
+	hiddenElement.target = '_blank';
+	hiddenElement.download = 'testing.csv';
+	hiddenElement.click();
+}
+
+// function WriteCSV(info) {
+// 	var csvRow = ""
+// 	csvRow += info.VNAME + ",";
+// 	//csvRow += info.VDATE + ",";
+// 	csvRow += info.WCOMM + ",";
+// 	//csvRow += info.HOURS + ",";
+// 	csvRow += info.OCOMM + ",";
+// 	csvRow += info.VPROJ;
+// 	csvRow += "\n";
+// 	console.log(csvRow);
+// }
+
+// function ReadCSV(data) {
+// 	//parse the csv first using split /n and then comma
+// 	//build dictionary based off of that 
+
+// 	var allTextLines = data.split(/\r\n|\n/);
+//     var headers = allTextLines[0].split(',');
+//     var lines = [];
+//     var dict2 = new Dictionary();
+
+//     for (var i=1; i<allTextLines.length; i++) {
+//         var data = allTextLines[i].split(',');
+//         if (data.length == headers.length) {
+//         	dict2.add(data[0], data[1]);
+//         }
+//     }
+// 	console.log(dict2);
+// }
+
+
+// function Dictionary() {
+// 	this.sets = [];
+
+// 	this.add = function(name, id) {
+// 		if (name && id) {
+// 			this.sets.push({
+// 				VNAME: name,
+// 				donor_id: id
+// 			});
+// 			return this.sets;
+// 		}
+// 	}
+
+// 	this.findID = function(name) {
+// 		for (var i = 0; i < this.sets.length; i++) {
+// 			if (this.sets[i].VNAME == name) {
+// 				return this.sets[i].donor_id;
+// 			}
+// 		}
+// 		return this.sets;
+// 	}
+
+// 	this.removeUser = function(name) {
+// 		for (var i = 0; i < this.sets.length; i++) {
+// 			if (this.sets[i].VNAME == name) {
+// 				this.sets[i].splice(this.sets[i], 1);
+// 			}
+// 		}
+// 		return this.sets;
+// 	}
+// }
