9b1c1768b036df048ea2cdcba46bed7a1ae20a7a
Patrick Wolfe <patrick.wolfe@tufts.edu>
2019-02-02 21:02:12 -0500

    Set up express server to host front end

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+app.use('/src/style', express.static(__dirname + '/src/style'));
+app.use('/src/html', express.static(__dirname + '/src/html'));
+app.use('/src/scripts', express.static(__dirname + '/src/scripts'));
+app.use('/src/assets', express.static(__dirname + '/src/assets'));
+app.use('/src/jquery-csv', express.static(__dirname + '/src/jquery-csv'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/src/html/login_logout_page.html')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
