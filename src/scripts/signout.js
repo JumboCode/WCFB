15de0f09eb27c9f18445130b71dac31fff37bcb1
harshoza24@gmail.com <harshoza24@gmail.com>
2019-03-10 14:10:09 -0400

    sends data to server and stores it

--- src/scripts/signout.js
+++ src/scripts/signout.js
@@ -100,9 +100,9 @@ function submitForm() {
 
 		WriteCSV(info, sendData);
 
-		// delete_name(INPUTS['VNAME']['val']);
+		delete_name(INPUTS['VNAME']['val']);
 		console.log(info)
-		// window.location.href = "login_logout_page.html"
+		window.location.href = "login_logout_page.html"
 	}
 }
 
