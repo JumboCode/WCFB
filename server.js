ad07846db013579fca20572e0bfb6e61cbc27090
Your Name <lulu.zheng@tufts.edu>
2019-04-07 14:42:02 -0400

    auth protecting endpoints

--- server.js
+++ server.js
@@ -1,22 +1,74 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const errorHandler = require('errorhandler');
+const auth = require('./src/routes/auth');
 
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
 const app = express();
 // note: enable cors only for testing - it can probably be removed later
 // for better security
 app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//app.use(express.static(path.join(__dirname, 'public')));
+// TODO (secret????)
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+// if (!isProduction) {
+//   app.use(errorHandler());
+// }
+
 app.options('GET', cors());
 const port = process.env.PORT || 3000;
-mongoose.Promise = global.Promise; mongoose.connect('mongodb://localhost:27017/WCFB');
+mongoose.Promise = global.Promise; mongoose.connect('mongodb://localhost:27017/wcfb');
+mongoose.set('debug', true);
+
+// Models and routes
+require('./src/models/Users');
+require('./src/config/passport');
+app.use(require('./src/routes'));
+
+//error handling is problematic gives 500 status
+//Error handlers & middlewares
+// if(!isProduction) {
+//   app.use((req, res, err) => {
+//     res.status(err.status || 500);
+//  res.json({
+//       errors: {
+//         message: err.message,
+//         error: err,
+//       },
+//     });
+//   });
+// }
+
+// app.use((req, res, err) => {
+//   res.status(err.status || 500);
+
+//   res.json({
+//     errors: {
+//       message: err.message,
+//       error: {"string" : "string"},
+//     },
+//   });
+// });
+
+//app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
 
 app.use('/src/style', express.static(`${__dirname}/src/style`));
 app.use('/src/html', express.static(`${__dirname}/src/html`));
 app.use('/src/scripts', express.static(`${__dirname}/src/scripts`));
 app.use('/src/assets', express.static(`${__dirname}/src/assets`));
-// app.use('/src/jquery-csv', express.static(__dirname + '/src/jquery-csv'));
+//app.use('/src/jquery-csv', express.static(__dirname + '/src/jquery-csv'));
 
-app.get('/', (req, res) => res.redirect('/src/html/login_logout_page.html'));
+//test if auth.required protects route
+app.get('/', auth.required,(req, res) => res.redirect('/src/html/login_logout_page.html'));
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
 
